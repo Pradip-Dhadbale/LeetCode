ad4552347dda7bd325277df2c3e30993a2b03fdb
Pradip <pradip88dhadbale@gmail.com>
2025-10-31 22:50:30 +0530

    Update palindrom.java

--- palindrom.java
+++ palindrom.java
@@ -1,1 +1,17 @@
-palindrome.java
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int temp = x;
+        int rev_no = 0;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            rev_no = rev_no * 10 + digit;
+            temp = temp / 10;
+        } 
+       return (x == rev_no);
+    }
+}
