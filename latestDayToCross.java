abda536b513156865c63915f395bf32aea48c6cb
Pradip Dhadbale <pradip88dhadbale@gmail.com>
2025-12-31 17:56:34 +0530

    latestDayToCross

---
+++ latestDayToCross.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Solution {
+    public int latestDayToCross(int row, int col, int[][] cells) {
+        int left = 0, right = cells.length;
+        int ans = 0;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (canCross(row, col, mid, cells)) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+    
+    private boolean canCross(int row, int col, int day, int[][] cells) {
+        int[][] grid = new int[row][col];
+        for (int i = 0; i < day; i++) {
+            grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
+        }
+        
+        Queue<int[]> queue = new LinkedList<>();
+        for (int c = 0; c < col; c++) {
+            if (grid[0][c] == 0) {
+                queue.offer(new int[]{0, c});
+                grid[0][c] = 1; 
+            }
+        }
+        
+        int[] dr = {0, 0, 1, -1};
+        int[] dc = {1, -1, 0, 0};
+        
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int r = curr[0];
+            int c = curr[1];
+            
+            if (r == row - 1) return true;
+            
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+                
+                if (nr >= 0 && nr < row && nc >= 0 && nc < col && grid[nr][nc] == 0) {
+                    grid[nr][nc] = 1;
+                    queue.offer(new int[]{nr, nc});
+                }
+            }
+        }
+        
+        return false;
+    }
+}
