a8125294047c7e53bf23b4270ce40f417d31c69f
Pradip Dhadbale <pradip88dhadbale@gmail.com>
2026-01-12 15:43:13 +0530

    minTimeToVisitAllPoints

---
+++ minTimeToVisitAllPoints.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minTimeToVisitAllPoints(int[][] points) {
+        int totalTime = 0;
+        
+        for (int i = 0; i < points.length - 1; i++) {
+            int x1 = points[i][0];
+            int y1 = points[i][1];
+            int x2 = points[i+1][0];
+            int y2 = points[i+1][1];
+            
+            // The time to travel between two points is the Chebyshev distance:
+            // max(|x2 - x1|, |y2 - y1|)
+            totalTime += Math.max(Math.abs(x2 - x1), Math.abs(y2 - y1));
+        }
+        
+        return totalTime;
+    }
+}
